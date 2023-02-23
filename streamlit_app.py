bf2f4661ad91de4b8d5d323730cb0dd76db10004
movingforward-taker <126141365+movingforward-taker@users.noreply.github.com>
2023-02-23 17:21:32 +1100

    Update streamlit_app.py
    Added header and text to the file for testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@ import streamlit
 
 streamlit.title('Cheers to the New Beginnings!')
 
+streamlit.header('Stop the mundane activities!')
+streamlit.text('Work on fitness goals')
+streamlit.text('Continue on your passion')
+streamlit.text('Stay true to your conscience')
+

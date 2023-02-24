60d318708614390a5d54d141e5347fa668a3bafd
movingforward-taker <126141365+movingforward-taker@users.noreply.github.com>
2023-02-24 13:36:41 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,13 @@ streamlit.title('Cheers to the New Beginnings!')
 
 streamlit.header('Start reading Fruits using Pandas :) !')
 
+# import pandas
 import pandas
+# call the read_csv function
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# Use streamlit dataframe to call the my_fruit_list object
 streamlit.dataframe(my_fruit_list)
+
+# Put a pick-list to select the fruit to include
+streamlit.multiselect('Pick some fruits:',list(my_fruit_list.index))
+

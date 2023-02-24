07a0dc5f0af3302dc537d91ff2edbc7c8e7b06ab
movingforward-taker <126141365+movingforward-taker@users.noreply.github.com>
2023-02-24 13:32:29 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,7 @@ import streamlit
 
 streamlit.title('Cheers to the New Beginnings!')
 
-streamlit.header('Stop the mundane activities!')
-streamlit.text('Work on fitness goals')
-streamlit.text('Continue on your passion')
-streamlit.text('Stay true to your conscience')
+streamlit.header('Start reading Fruits using Pandas :) !')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")

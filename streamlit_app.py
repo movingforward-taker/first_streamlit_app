477b834866fb0b8c67cf70be3a38ed15ab13953d
movingforward-taker <126141365+movingforward-taker@users.noreply.github.com>
2023-02-24 13:30:58 +1100

    Update streamlit_app.py
    24-Feb-2023:
    Import pandas to read csv file and use streamlit to show the dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,6 @@ streamlit.text('Work on fitness goals')
 streamlit.text('Continue on your passion')
 streamlit.text('Stay true to your conscience')
 
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)

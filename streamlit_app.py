58474cffd7f026aff5f236105d5cf38f21d408c2
movingforward-taker <126141365+movingforward-taker@users.noreply.github.com>
2023-02-24 13:39:41 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,7 @@ streamlit.header('Start reading Fruits using Pandas :) !')
 import pandas
 # call the read_csv function
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 # Use streamlit dataframe to call the my_fruit_list object
 streamlit.dataframe(my_fruit_list)
 

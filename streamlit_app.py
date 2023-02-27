8955e3bb48893293a23f0cf91f79d762520cefb3
movingforward-taker <126141365+movingforward-taker@users.noreply.github.com>
2023-02-28 10:49:15 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,5 +24,11 @@ streamlit.dataframe(fruits_to_show)
 # new section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 import requests
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#streamlit.text(fruityvice_response.json())
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+#normalizing the incoming json data for the api endpoint
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#output in screen as table
+streamlit.dataframe(fruityvice_normalized)

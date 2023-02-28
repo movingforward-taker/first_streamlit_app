db09119201708d88f25d814d7029662d0f05d546
movingforward-taker <126141365+movingforward-taker@users.noreply.github.com>
2023-02-28 14:34:43 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,9 +23,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # streamlit.multiselect('Pick some fruits:',list(my_fruit_list.index),['Avocado','Strawberries'])
 streamlit.dataframe(fruits_to_show)
 
-# new section to display fruityvice api response
-streamlit.header('Fruityvice Fruit Advice!')
-
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #streamlit.text(fruityvice_response.json())
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
@@ -40,15 +37,19 @@ streamlit.header('Fruityvice Fruit Advice!')
 #output in screen as table
 #streamlit.dataframe(fruityvice_normalized)
 
+#create a function to group the repeatable code block
+def get_fruityvicedata(this_fruit_choice)
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      return fruityvice_normalized
+streamlit.header('Fruityvice Fruit Advice!')
 try:
   fruit_choice = streamlit.text_input('What Fruit would you like information about?')
   if not fruit_choice:
       streamlit.error('Please select a fruit for information')
   else:
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-      streamlit.dataframe(fruityvice_normalized)
-
+      back_from_function = get_fruityvice_data(fruit_choice)
+      streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
 

dda3545b8eed7fdd8a3a9ef28b316018b1e3089e
movingforward-taker <126141365+movingforward-taker@users.noreply.github.com>
2023-02-28 14:39:14 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ try:
   if not fruit_choice:
       streamlit.error('Please select a fruit for information')
   else:
-      back_from_function = get_fruityvice_data(fruit_choice)
+      back_from_function = get_fruityvicedata(fruit_choice)
       streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
